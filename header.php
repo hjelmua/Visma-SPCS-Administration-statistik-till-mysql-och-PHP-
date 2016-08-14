a5ff1fdd24e28bbc1e40909dd1f2a1c819ad9980
hjelmua <jonas.hjelm@pentarex.se>
2016-08-14 11:00:26 +0200

    moved scripts to top for better support for charts

--- header.php
+++ header.php
@@ -6,27 +6,28 @@
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.6 -->
-  <link rel="stylesheet" href="/AdminLTE/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="AdminLTE/bootstrap/css/bootstrap.min.css">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
   <!-- DataTables -->
-   <link rel="stylesheet" href="/AdminLTE/plugins/datatables/dataTables.bootstrap.css">
+   <link rel="stylesheet" href="AdminLTE/plugins/datatables/dataTables.bootstrap.css">
   <!-- bootstrap datepicker -->
-    <link rel="stylesheet" href="/AdminLTE/plugins/datepicker/datepicker3.css">
+    <link rel="stylesheet" href="AdminLTE/plugins/datepicker/datepicker3.css">
 <!-- Bootstrap time Picker -->
-  <link rel="stylesheet" href="/AdminLTE/plugins/timepicker/bootstrap-timepicker.min.css">
+  <link rel="stylesheet" href="AdminLTE/plugins/timepicker/bootstrap-timepicker.min.css">
 <!-- Select2 -->
-  <link rel="stylesheet" href="/AdminLTE/plugins/select2/select2.min.css">
-  
+  <link rel="stylesheet" href="AdminLTE/plugins/select2/select2.min.css">
+  <!-- Morris charts -->
+  <link rel="stylesheet" href="AdminLTE/plugins/morris/morris.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="/AdminLTE/dist/css/AdminLTE.min.css">
+  <link rel="stylesheet" href="AdminLTE/dist/css/AdminLTE.min.css">
   <!-- AdminLTE Skins. We have chosen the skin-blue for this starter
         page. However, you can choose any other skin. Make sure you
         apply the skin class to the body tag so the changes take effect.
   -->
-  <link rel="stylesheet" href="/AdminLTE/dist/css/skins/skin-blue-light.min.css">
+  <link rel="stylesheet" href="AdminLTE/dist/css/skins/skin-blue-light.min.css">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -35,9 +36,37 @@
   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
   <![endif]-->
 </head>
-<script src="/AdminLTE/plugins/chartjs/Chart.min.js"></script>
+<!-- jQuery 2.2.3 -->
+<script src="AdminLTE/plugins/jQuery/jquery-2.2.3.min.js"></script>
+<!-- Bootstrap 3.3.6 -->
+<script src="AdminLTE/bootstrap/js/bootstrap.min.js"></script>
+<!-- Morris.js charts -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+<script src="AdminLTE/plugins/morris/morris.min.js"></script>
+
+
+
+<!-- bootstrap datepicker -->
+<script src="AdminLTE/plugins/datepicker/bootstrap-datepicker.js"></script>
+
+<!-- SlimScroll -->
+<script src="AdminLTE/plugins/slimScroll/jquery.slimscroll.min.js"></script>
+<!-- FastClick -->
+<script src="AdminLTE/plugins/fastclick/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="AdminLTE/dist/js/app.min.js"></script>
+
+
+<!-- Optionally, you can add Slimscroll and FastClick plugins.
+     Both of these plugins are recommended to enhance the
+     user experience. Slimscroll is required when using the
+     fixed layout. -->
+<!-- DataTables -->
+<script src="AdminLTE/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="AdminLTE/plugins/datatables/dataTables.bootstrap.min.js"></script>
 <body class="hold-transition skin-blue-light sidebar-mini">
 <div class="wrapper">
+
 
   <!-- Main Header -->
   <header class="main-header">
